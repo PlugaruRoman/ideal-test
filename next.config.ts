9a658bc871a78f9053232849f2a50269bb8afde6
Roman Plugaru <roman.plugaru@internal.ebs.md>
2025-03-04 10:52:16 +0200

    fix: fix yml

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,7 @@ const nextConfig: NextConfig = {
   ],
 
   output: 'standalone',
+  distDir: 'out',
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
     turbo: {
